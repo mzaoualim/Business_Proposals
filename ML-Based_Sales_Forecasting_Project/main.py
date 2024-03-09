b3ed781069d14b0f72685aabcec537dd1c5165c7
mzaoualim <74496123+mzaoualim@users.noreply.github.com>
2024-03-09 16:06:51 +0100

    Update main.py

--- ML-Based_Sales_Forecasting_Project/main.py
+++ ML-Based_Sales_Forecasting_Project/main.py
@@ -68,9 +68,10 @@ def main():
         plt.figure(figsize=(10, 6))
         fig, ax = plt.subplots()
         ax1 = sn.lineplot(data=data_test, x=data_test['ds'], y=data_test['y'])
+        ax1.set_ylabel('Total Sales', rotation=0)
+        ax1.set_xlabeldata_test['ds'], rotation=90)
         ax2 = sn.lineplot(data=preds, x=preds['ds'], y=preds['yhat'])
-        plt.xticks(rotation=45)
-        plt.ylabel('Total Sales')
+       
         plt.title('Predicted vs Actuals')
         st.pyplot(fig, use_container_width=True)
 
